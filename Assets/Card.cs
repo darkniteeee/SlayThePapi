ffaa720b763321ede5334f2cc34c57feaaebfcbb
Thomas <thomas.voltz@outlook.fr>
2022-11-25 19:03:25 +0100

    bidouillage initial

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -7,7 +7,7 @@ public class Card : MonoBehaviour
     private int id;
     private string name;
     private int cost;
-    private int power;
+    private int damage;
     private string description;
 
     public Card() 
@@ -15,14 +15,70 @@ public class Card : MonoBehaviour
 
     }
 
-     public Card(int id, string name, int cost,int power,string description) 
+     public Card(int id, string name, int cost, int damage, string description) 
+    {
+        SetId(id);
+        SetName(name);
+        SetCost(cost);
+        SetDamage(damage);
+        SetDescription(description);
+    }
+
+    // getter
+    public int GetId() 
+    {
+        return this.id;
+    }
+
+    public string GetName() 
+    {
+        return this.name;
+    }
+
+    public int GetCost() 
+    {
+        return this.cost;
+    }
+
+    public int GetDamage() 
+    {
+        return this.damage;
+    }
+
+    public string GetDescription()  
+    {
+        return this.description;
+    }
+
+    // setter
+    public void SetId(int id)
     {
         this.id = id;
-        this.name = name;
-        this.cost = cost;
-        this.power = power;
-        this.description = description;
     }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetCost(int cost)
+    {
+        this.cost = cost;
+    }
+
+    public void SetDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
+    public void SetDescription(string description) 
+    { 
+        this.description = description; 
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,4 +90,27 @@ public class Card : MonoBehaviour
     {
         
     }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Card;*/
+
+public class CardDatabase : MonoBehaviour
+{
+    public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        cardList.Add(new Card(0,"Bullet",1,2,$"inflige {this.GetDamage()} dégats"));
+        cardList.Add(new Card(1,"Fire Bullet",1,3,$"inflige {damage} dégats et brulure"));
+        cardList.Add(new Card(2,"Electric Bullet",1,2,$"inflige {damage} dégats et Shock"));
+        cardList.Add(new Card(3,"Poison Bullet",1,2,$"inflige {damage} dégats et poison"));
+        cardList.Add(new Card(4,"Honey Bullet",1,2,$"inflige {damage} dégats et honeyed"));
+        cardList.Add(new Card(5,"Water Bullet",1,1,$"inflige {damage} dégats et wet"));
+        cardList.Add(new Card(6,"Necro-Bullet",1,2,$"inflige {damage} dégats et fear"));
+        cardList.Add(new Card(7,"Light-Bullet",1,2,$"inflige {damage} dégats et lighted"));
+    }
+}
+
+
 }
--- Assets/CardDatabase.cs
+++ Assets/CardDatabase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Card;
 
 public class CardDatabase : MonoBehaviour
 {
@@ -8,14 +9,14 @@ public class CardDatabase : MonoBehaviour
 
     void Awake()
     {
-        cardList.Add(new Card(0,"Bullet",1,2,$"inflige {power} dégats"));
-        cardList.Add(new Card(1,"Fire Bullet",1,3,$"inflige {power} dégats et brulure"));
-        cardList.Add(new Card(2,"Electric Bullet",1,2,$"inflige {power} dégats et Shock"));
-        cardList.Add(new Card(3,"Poison Bullet",1,2,$"inflige {power} dégats et poison"));
-        cardList.Add(new Card(4,"Honey Bullet",1,2,$"inflige {power} dégats et honeyed"));
-        cardList.Add(new Card(5,"Water Bullet",1,1,$"inflige {power} dégats et wet"));
-        cardList.Add(new Card(6,"Necro-Bullet",1,2,$"inflige {power} dégats et fear"));
-        cardList.Add(new Card(7,"Light-Bullet",1,2,$"inflige {power} dégats et lighted"));
+        cardList.Add(new Card(0,"Bullet",1,2,$"inflige {this.GetDamage()} dégats"));
+        cardList.Add(new Card(1,"Fire Bullet",1,3,$"inflige {damage} dégats et brulure"));
+        cardList.Add(new Card(2,"Electric Bullet",1,2,$"inflige {damage} dégats et Shock"));
+        cardList.Add(new Card(3,"Poison Bullet",1,2,$"inflige {damage} dégats et poison"));
+        cardList.Add(new Card(4,"Honey Bullet",1,2,$"inflige {damage} dégats et honeyed"));
+        cardList.Add(new Card(5,"Water Bullet",1,1,$"inflige {damage} dégats et wet"));
+        cardList.Add(new Card(6,"Necro-Bullet",1,2,$"inflige {damage} dégats et fear"));
+        cardList.Add(new Card(7,"Light-Bullet",1,2,$"inflige {damage} dégats et lighted"));
     }
 }
 
