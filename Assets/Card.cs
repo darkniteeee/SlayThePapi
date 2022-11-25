602ef53bd59e334e2c5f241403c2a198be52e3a8
Thomas <thomas.voltz@outlook.fr>
2022-11-25 19:03:35 +0100

    idem

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -90,27 +90,5 @@ public class Card : MonoBehaviour
     {
         
     }
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Card;*/
-
-public class CardDatabase : MonoBehaviour
-{
-    public static List<Card> cardList = new List<Card>();
-
-    void Awake()
-    {
-        cardList.Add(new Card(0,"Bullet",1,2,$"inflige {this.GetDamage()} dégats"));
-        cardList.Add(new Card(1,"Fire Bullet",1,3,$"inflige {damage} dégats et brulure"));
-        cardList.Add(new Card(2,"Electric Bullet",1,2,$"inflige {damage} dégats et Shock"));
-        cardList.Add(new Card(3,"Poison Bullet",1,2,$"inflige {damage} dégats et poison"));
-        cardList.Add(new Card(4,"Honey Bullet",1,2,$"inflige {damage} dégats et honeyed"));
-        cardList.Add(new Card(5,"Water Bullet",1,1,$"inflige {damage} dégats et wet"));
-        cardList.Add(new Card(6,"Necro-Bullet",1,2,$"inflige {damage} dégats et fear"));
-        cardList.Add(new Card(7,"Light-Bullet",1,2,$"inflige {damage} dégats et lighted"));
-    }
-}
-
-
+    
 }
