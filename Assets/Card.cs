1c6c05b259792bf2adadb352828c0e92240439c4
Thomas <thomas.voltz@outlook.fr>
2022-11-25 18:15:28 +0100

    Card et card database test

---
+++ Assets/Card.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private int id;
+    private string name;
+    private int cost;
+    private int power;
+    private string description;
+
+    public Card() 
+    {
+
+    }
+
+     public Card(int id, string name, int cost,int power,string description) 
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CardDatabase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDatabase : MonoBehaviour
+{
+    public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        cardList.Add(new Card(0,"Bullet",1,2,$"inflige {power} dégats"))
+        cardList.Add(new Card(1,"Fire Bullet",1,3,$"inflige {power} dégats et brulure"))
+        cardList.Add(new Card(2,"Electric Bullet",1,2,$"inflige {power} dégats et Shock"))
+        cardList.Add(new Card(3,"Poison Bullet",1,2,$"inflige {power} dégats et poison"))
+        cardList.Add(new Card(4,"Honey Bullet",1,2,$"inflige {power} dégats et honeyed"))
+        cardList.Add(new Card(5,"Water Bullet",1,1,$"inflige {power} dégats et wet"))
+        cardList.Add(new Card(6,"Necro-Bullet",1,2,$"inflige {power} dégats et fear"))
+        cardList.Add(new Card(7,"Light-Bullet",1,2,$"inflige {power} dégats et lighted"))
+
+    }
+}
+
